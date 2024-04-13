d8f6500eec066f0d8aadb43a728fd750eadf37e0
Charudatta Ghute <122104600+CharudattaGhute@users.noreply.github.com>
2024-04-14 01:15:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,78 +1,81 @@
-function uppcase() {
-  const upper = document.getElementById("textarea").value;
-  let result = upper.toUpperCase();
-  document.getElementById("textarea").value = result;
+let edit = [];
+function submit() {
+  const id = document.getElementById("Id1").value;
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const price = document.getElementById("price").value;
+  const quantity = document.getElementById("quantity").value;
+
+  const contentdiv = document.getElementById("booklist");
+  const newDiv = document.createElement("tr");
+
+  newDiv.innerHTML = `
+    
+              <td>${id}</td>
+              <td>${title}</td>
+              <td>${author}</td>
+              <td>${price}</td>
+              <td>${quantity}</td>
+              <td>
+                <button class="btn btn-primary" onclick='editBook(this)'>Edit</button>
+                <button class="btn btn-danger" onclick='deleteBook(this)'>Delete</button>
+              </td>
+          
+                          `;
+
+  contentdiv.appendChild(newDiv);
+
+  document.getElementById("Id1").value = "";
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("price").value = "";
+  document.getElementById("quantity").value = "";
+  edit.push({ title: title, author: author, price: price, quantity: quantity });
+}
+function editBook() {
+  const editModal = document.getElementById("editModal");
+
+  $(editModal).modal("show");
+}
+function deleteBook(element) {
+  const row = element.closest("tr");
+  row.remove();
 }
 
-function lowercase() {
-  const lower = document.getElementById("textarea").value;
-  let resultlower = lower.toLowerCase();
-  document.getElementById("textarea").value = resultlower;
-}
-function trim() {
-  const trimchr = document.getElementById("textarea").value;
-  let resulttrim = trimchr.trim();
-  document.getElementById("textarea").value = resulttrim;
-}
-function concat() {
-  const concatstr = document.getElementById("textarea").value;
-  const concatstr2 = prompt("Enter the string:");
-  let resultconcat = concatstr.concat(concatstr2);
-  document.getElementById("textarea").value = resultconcat;
-  console.log(resultconcat);
+function editcontent() {
+  const title1 = document.getElementById("title1").value;
+  const author1 = document.getElementById("author1").value;
+  const price1 = document.getElementById("price1").value;
+  const quantity1 = document.getElementById("quantity1").value;
+
+  const secondedit = document.getElementById("booklist");
+
+  const editDiv = document.createElement("tr");
+
+  editDiv.innerHTML = `
+
+             <td>${title1}</td>
+               <td>${author1}</td>
+              <td>${price1}</td>
+              <td>${quantity1}</td>
+              <button class="btn btn-danger" onclick='deleteBook(this)'>Delete</button>
+
+              `;
+
+  secondedit.appendChild(editDiv);
+  document.getElementById("booklist").value = title1;
+  document.getElementById("booklist").value = author1;
+  document.getElementById("booklist").value = price1;
+  document.getElementById("booklist").value = quantity1;
+  $(editModal).modal("hide");
 }
 
-function slice() {
-  const slicestr = document.getElementById("textarea").value;
-  const inputindex = prompt("ENter the index :");
+// function editcontent() {
+//   document.getElementById("booklist").value;
 
-  const start = parseInt(inputindex);
-  const end = start + 1;
-
-  const resultslice = slicestr.slice(start, end);
-  document.getElementById("textarea").value = resultslice;
-}
-function length() {
-  const strlength = document.getElementById("textarea").value;
-  let resultlength = strlength.length;
-  document.getElementById("textarea").value = resultlength;
-}
-function split() {
-  const splitstr = document.getElementById("textarea").value;
-  let resultsplit = splitstr.split("");
-  document.getElementById("textarea").value = resultsplit;
-}
-function charat() {
-  const chrstr = document.getElementById("textarea").value;
-  let resultchr = prompt("Enter the inde:");
-
-  let chrstrart = chrstr.charAt(resultchr);
-  document.getElementById("textarea").value = chrstrart;
-}
-function countChar() {
-  const text = document.getElementById("textarea").value;
-  const charToCount = prompt("Enter the character to count:");
-  const counttext = (text.match(new RegExp(charToCount, "g")) || []).length;
-  document.getElementById("textarea").value = counttext;
-}
-function replace() {
-  const replacestr = document.getElementById("textarea").value;
-  const resultreplace = replacestr.replaceAll("the", "is");
-  document.getElementById("textarea").value = resultreplace;
-}
-function hello() {
-  let result = document.getElementById("textarea").value;
-  let vouwelcount = 0;
-  for (const char of result) {
-    if (
-      char === "a" ||
-      char === "e" ||
-      char === "i" ||
-      char === "o" ||
-      char === "u"
-    ) {
-      vouwelcount++;
-    }
-  }
-  document.getElementById("textarea").value = vouwelcount;
-}
+//   edit.forEach(function submit() {
+//     newarr.push(edit);
+//     console.log(edit);
+//     console.log("newarr", newarr);
+//   });
+// }
